51d00b8a8a9906413b58699ca2ec3688dca4f02f
Your Name <your-email@example.com>
2025-11-18 12:53:06 +0530

    Add: splashPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/Router";
 import "./App.css";
-
+import { SplashPage } from "./pages/SplashPage";
 
 export const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setIsLoading(false), 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
-      <RouterProvider router={router} />
+      {isLoading ? <SplashPage/> : <RouterProvider router={router} />}
     </>
   );
 };
---
+++ src/pages/SplashPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const SplashPage = () => {
+  return (
+    <>
+      <div className="h-[100vh] flex items-center justify-center bg-white">
+        <img
+          src="/svgs/brand-logo/logo-trazi.svg"
+          alt="logo"
+          width="250"
+          className="object-contain animate-pulse"
+        />
+      </div>
+    </>
+  );
+};
